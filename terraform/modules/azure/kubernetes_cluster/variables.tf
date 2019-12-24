114f6eed04bfd10713d6f2fc73fb2a0162cf1bed
Marat Bediev <bediev@gmail.com>
2019-12-24 15:28:24 +0600

    Tue 24 Dec 2019 03:28:24 PM +06

--- terraform/modules/azure/kubernetes_cluster/variables.tf
+++ terraform/modules/azure/kubernetes_cluster/variables.tf
@@ -1,34 +1,34 @@
 variable "name_prefix" {
-  description = "The prefix for the resources created in the specified Azure Resource Group"
+  description = "The prefix for the resources created in the specified Azure Resource Group."
 }
 
 variable "location" {
   default     = "eastus"
-  description = "The location for the AKS deployment"
+  description = "The location for the AKS deployment."
 }
 
 variable "admin_username" {
   default     = "azureuser"
-  description = "The username of the local administrator to be created on the Kubernetes cluster"
+  description = "The username of the local administrator to be created on the Kubernetes cluster."
 }
 
 variable "agents_size" {
   default     = "Standard_F2"
-  description = "The default virtual machine size for the Kubernetes agents"
+  description = "The default virtual machine size for the Kubernetes agents."
 }
 
 variable "log_analytics_workspace_sku" {
-  description = "The SKU (pricing level) of the Log Analytics workspace. For new subscriptions the SKU should be set to PerGB2018"
+  description = "The SKU (pricing level) of the Log Analytics workspace. For new subscriptions the SKU should be set to PerGB2018."
   default     = "PerGB2018"
 }
 
 variable "log_retention_in_days" {
-  description = "The retention period for the logs in days"
+  description = "The retention period for the logs in days."
   default     = 30
 }
 
 variable "agents_count" {
-  description = "The number of Agents that should exist in the Agent Pool"
+  description = "The number of Agents that should exist in the Agent Pool."
   default     = 2
 }
 
@@ -38,6 +38,6 @@ variable "kubernetes_version" {
 }
 
 variable "public_ssh_key" {
-  description = "A custom ssh key to control access to the AKS cluster"
+  description = "A custom ssh key to control access to the AKS cluster.  If no public ssh key is set through variables, a newly generated public key will be used and the private key will be saved in a private_ssh_key file."
   default     = ""
 }
